6b9c199518650bacf04e2560ce030bc0e9920edd
Eduardo Enriquez <eduardo.a.enriquez@gmail.com>
2018-04-01 17:30:01 -0300

    working on expenses

--- website/expenses/views.py
+++ website/expenses/views.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+from django.db.models.functions import TruncMonth
+from django.db.models import Sum
+
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.views import APIView
 
 from .serializers import ExpenseSerializer, CategorySerializer
 from .models import Expense, Category
@@ -15,3 +20,12 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.order_by('created_date').all()
     serializer_class = CategorySerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+
+class CollectSubmissionsView(APIView):
+    def get(self, ):
+        today = datetime.now()
+        queryset = Expense.objects.filter(
+            created_date__year=today.year,
+            created_date__month=today.month
+        ).aggregate(Sum('amount'))
